1b24cdf1fdda6aaa4020b5510dead074c3cd5552
NestorNey <nestor.ascencio1418@alumnos.udg.mx>
2023-05-24 21:21:37 -0600

    Verificador de actualizaciones agregado

--- app/src/main/java/com/myroute/dbmanager/DBManager.kt
+++ app/src/main/java/com/myroute/dbmanager/DBManager.kt
@@ -1,16 +1,51 @@
 package com.myroute.dbmanager
 
 import android.annotation.SuppressLint
-import android.content.ContentValues
-import android.content.Context
-import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+import com.myroute.MainActivity
 import com.myroute.models.Colonia
 import com.myroute.models.Ruta
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import okhttp3.Call
+import okhttp3.Callback
 import org.osmdroid.util.GeoPoint
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.concurrent.TimeUnit
 
-class DBManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class DBManager(val context: MainActivity) : SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    // Variable para verificar si el proceso (corrutina) de verificacion de actualizaciones a finalizado
+    private var isCFUCompleted: Boolean = false
+
+    // Variable para verificar si la base de datos ha sido o esta actualizada
+    private var isUpdated: Boolean = false
+
+    init {
+        // Creacion de corrutina para la verificacion de actualizaciones de la base de datos
+        GlobalScope.launch (Dispatchers.IO) {
+
+            // Inicion de proceso de verficacion de actualizaciones de la base de datos
+            isUpdated = checkForUpdates()
+
+            if (!isUpdated){
+                Log.w("MyRoute:DBManager/Update", "No se pudo completar la verificacion de " +
+                        "actualizaciones, puede que este usando una base de datos desactualizada")
+            }
+
+            // Se marca como completo el proceso de verificacion de actualzaciones de la base de datos
+            isCFUCompleted = true
+        }
+    }
 
     //Informacion de la base de datos
     companion object {
@@ -30,6 +65,7 @@ class DBManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
 
     //--------Metodos para el manejo de la base de datos--------//
     override fun onCreate(db: SQLiteDatabase?) {
+
         val createRutasTable = "CREATE TABLE $TABLE_NAME_RUTAS ($COLUMN_ID_ROUTE TEXT PRIMARY KEY, $COLUMN_REF_POINTS TEXT, $COLUMN_REF_STOPS TEXT, $COLUMN_COLOR TEXT)"
         val createColoniasTable = "CREATE TABLE $TABLE_NAME_COLONIAS ($COLUMN_ID_COLN TEXT PRIMARY KEY, $COLUMN_REF_ROUTS TEXT, $COLUMN_COLN_RADIO REAL, $COLUMN_COLN_COLIN TEXT)"
         db?.execSQL(createRutasTable)
@@ -44,50 +80,11 @@ class DBManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
     //----------------------------------------------------------//
 
     //-------------Metodos para obtener los modelos-------------//
-    fun addRoute(idRoute: String, refPoints: ArrayList<DoubleArray>, refStops: ArrayList<DoubleArray>, color: String) {
-        val db = this.writableDatabase
-
-        // Checamos si la ruta existe
-        val cursor: Cursor = db.rawQuery("SELECT * FROM $TABLE_NAME_RUTAS WHERE $COLUMN_ID_ROUTE=?", arrayOf(idRoute))
-        if (cursor.count == 0) {
-            // Si no existe insertamos la ruta en la tabla
-            val values = ContentValues()
-            values.put(COLUMN_ID_ROUTE, idRoute)
-
-            // Convertimos los puntos de referencia a string
-            val refPointsStr = refPoints.joinToString("|") { point -> "${point[0]},${point[1]}" }
-            values.put(COLUMN_REF_POINTS, refPointsStr)
-
-            // Convertimos los puntos de parada a string
-            val refStopsStr = refStops.joinToString("|") { point -> "${point[0]},${point[1]}" }
-            values.put(COLUMN_REF_STOPS, refStopsStr)
-
-            values.put(COLUMN_COLOR, color)
-
-            db.insert(TABLE_NAME_RUTAS, null, values)
-        } else {
-            // Si existe solo actualizamos la ruta
-            val values = ContentValues()
-
-            // Convertimos los puntos de referencia a string
-            val refPointsStr = refPoints.joinToString("|") { point -> "${point[0]},${point[1]}" }
-            values.put(COLUMN_REF_POINTS, refPointsStr)
-
-            // Convertimos los puntos de parada a string
-            val refStopsStr = refStops.joinToString("|") { point -> "${point[0]},${point[1]}" }
-            values.put(COLUMN_REF_STOPS, refStopsStr)
-
-            values.put(COLUMN_COLOR, color)
-
-            db.update(TABLE_NAME_RUTAS, values, "$COLUMN_ID_ROUTE=?", arrayOf(idRoute))
-        }
-
-        cursor.close()
-        db.close()
-    }
 
     @SuppressLint("Range")
     fun getRoute(idRoute: String): Ruta? {
+        if (!isCFUCompleted)return null
+
         val db = this.readableDatabase
 
         val cursor = db.rawQuery("SELECT $COLUMN_REF_POINTS, $COLUMN_REF_STOPS, $COLUMN_COLOR FROM $TABLE_NAME_RUTAS WHERE $COLUMN_ID_ROUTE=?", arrayOf(idRoute))
@@ -129,7 +126,9 @@ class DBManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
     }
 
     @SuppressLint("Range")
-    fun getAllRoutes(): ArrayList<Ruta> {
+    fun getAllRoutes(): ArrayList<Ruta>? {
+        if (!isCFUCompleted)return null
+
         val routes = ArrayList<Ruta>()
         val db = this.readableDatabase
 
@@ -151,42 +150,10 @@ class DBManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         return routes
     }
 
-    fun addColonia(idColn: String, refRouts: Array<String>, colnColin: Array<String>) {
-        val db = this.writableDatabase
-
-        // Check if the colonia already exists
-        val cursor: Cursor = db.rawQuery("SELECT * FROM $TABLE_NAME_COLONIAS WHERE $COLUMN_ID_COLN=?", arrayOf(idColn))
-        if (cursor.count == 0) {
-            // If the colonia doesn't exist, insert it into the table
-            val values = ContentValues()
-            values.put(COLUMN_ID_COLN, idColn)
-
-            // Convert refRouts and colnColin arrays to strings
-            val refRoutsStr = refRouts.joinToString("|")
-            val colnColinStr = colnColin.joinToString("|")
-            values.put(COLUMN_REF_ROUTS, refRoutsStr)
-            values.put(COLUMN_COLN_COLIN, colnColinStr)
-
-            db.insert(TABLE_NAME_COLONIAS, null, values)
-        } else {
-            // If the colonia already exists, update its refRouts and colnColin
-            val values = ContentValues()
-
-            // Convert refRouts and colnColin arrays to strings
-            val refRoutsStr = refRouts.joinToString("|")
-            val colnColinStr = colnColin.joinToString("|")
-            values.put(COLUMN_REF_ROUTS, refRoutsStr)
-            values.put(COLUMN_COLN_COLIN, colnColinStr)
-
-            db.update(TABLE_NAME_COLONIAS, values, "$COLUMN_ID_COLN=?", arrayOf(idColn))
-        }
-
-        cursor.close()
-        db.close()
-    }
-
     @SuppressLint("Range")
     fun getColonia(idColn: String): Colonia? {
+        if (!isCFUCompleted)return null
+
         val db = this.readableDatabase
 
         val cursor = db.rawQuery("SELECT * FROM $TABLE_NAME_COLONIAS WHERE $COLUMN_ID_COLN=?", arrayOf(idColn))
@@ -211,4 +178,176 @@ class DBManager(context: Context) : SQLiteOpenHelper(context, DATABASE_NAME, nul
         return Colonia(idColn, refRoutsArray, colnColinArray, colnRadio)
     }
     //----------------------------------------------------------//
+
+    //Funciones para verificar actualizaciones para la base de datos//
+    private suspend fun checkForUpdates() : Boolean{
+
+        var timeout = 0
+
+        val userVersion = getUserVersion()
+        val githubVersion = getGitHubVersion()
+
+        if (githubVersion == null){
+            Log.e("MyRoute:DBManager/Update", "La version (string) obtenida de github es nula")
+            return false
+        }
+
+        if (userVersion != githubVersion){
+            updateDatabase()
+            while (!isUpdated){
+                delay(1000L)
+                timeout+=1
+                Log.i("MyRoute:DBManager/Update", "Esperando al proceso de actaulizacion, tiempo transcurrido: " + timeout + " segundos")
+                if(timeout >= 90){
+                    Log.e("MyRoute:DBManager/Update", "El tiempo de espera sobrepaso los 90 segundos")
+                    break
+                }
+            }
+            if (isUpdated){
+                modifyVersionTextFile(githubVersion)
+                Log.i("MyRoute:DBManager/Update", "Se logro actualizar la base de datos con exito")
+            }else{
+                return false
+            }
+        }else{
+            Log.i("MyRoute:DBManager/Update", "La base de datos esta actualizada, no requiere ningun cambio")
+        }
+
+        return true
+    }
+
+    private fun getUserVersion() : String?{
+        return try {
+            val file = File(context.filesDir, "version.txt")
+
+            if (!file.exists()) {
+                file.createNewFile()
+                return "noversion"
+            }
+
+            val inputStream = FileInputStream(file)
+            val size = inputStream.available()
+            val buffer = ByteArray(size)
+            inputStream.read(buffer)
+            inputStream.close()
+            String(buffer, Charsets.UTF_8)
+        } catch (e: Exception) {
+            Log.e("MyRoute:DBManager/Update", "Hubo un error al obtener la versión del usuario: " + e.message)
+            null
+        }
+    }
+    private fun getGitHubVersion() : String?{
+        val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("https://raw.githubusercontent.com/Mathle-ML/MyRoute/database/version.txt")
+            .build()
+
+        val response = client.newCall(request).execute()
+
+        return response.body?.string()
+    }
+
+    private fun updateDatabase() {
+        val databasePath = context.getDatabasePath("db.sqlite3")
+        if (!databasePath.exists()) {
+            // La base de datos no existe, crearla primero
+            val isDatabaseCreated = createDatabase(databasePath)
+            if (!isDatabaseCreated) {
+                // Muestra en LOG:ERROR diciendo que no se pudo crear la base de datos
+                Log.e("MyRoute:DBManager/Update", "No se pudo crear la base de datos en la ubicación deseada")
+                return
+            }
+        }
+
+        val url = "https://raw.githubusercontent.com/Mathle-ML/MyRoute/database/db.sqlite3"
+        val client = OkHttpClient.Builder()
+            .connectTimeout(90, TimeUnit.SECONDS) // Establecer tiempo límite de espera en segundos
+            .build()
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                // Muestra un LOG:ERROR con mensaje de error al no poder conectar
+                Log.e("MyRoute:DBManager/Update", "No se pudo conectar con la URL: " + e.message)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    val inputStream = response.body?.byteStream()
+                    if (inputStream != null) {
+
+                        // Guarda el archivo en el directorio principal de la aplicación
+                        val outputFile = File(context.filesDir, "db.sqlite3")
+                        val outputStream = FileOutputStream(outputFile)
+                        inputStream.copyTo(outputStream)
+                        outputStream.close()
+                        inputStream.close()
+
+                        // Llamar al método para copiar la base de datos desde el archivo descargado
+                        // a la ubicación deseada en la aplicación
+                        val externalDbFile = File(outputFile.absolutePath)
+                        val internalDbPath = databasePath.absolutePath
+
+                        try {
+                            val inputChannel = FileInputStream(externalDbFile).channel
+                            val outputChannel = FileOutputStream(internalDbPath).channel
+
+                            outputChannel.transferFrom(inputChannel, 0, inputChannel.size())
+
+                            inputChannel.close()
+                            outputChannel.close()
+
+                            // ¡La base de datos ha sido copiada exitosamente!
+                            isUpdated = true
+
+                            // Eliminar la base de datos descargada
+                            outputFile.delete()
+                        } catch (e: IOException) {
+                            // Manejar error de copia de la base de datos
+                            Log.e("MyRoute:DBManager/Update", "No se pudo copiar la base de datos: " + e.message)
+                            outputFile.delete()
+                        }
+                    } else {
+                        // Muestra en LOG:ERROR diciendo que el archivo es nulo
+                        Log.e("MyRoute:DBManager/Update", "La base de datos descargada es NULA")
+                    }
+                } else {
+                    // Muestra un LOG:ERROR con mensaje de error al no poder descargar la base de datos
+                    Log.e("MyRoute:DBManager/Update", "No se pudo descargar la base de datos: " + response.message)
+                }
+            }
+        })
+    }
+
+    private fun createDatabase(databasePath: File): Boolean {
+        return try {
+            // Crea la base de datos vacía en la ubicación deseada
+            val outputChannel = FileOutputStream(databasePath).channel
+            outputChannel.close()
+            true
+        } catch (e: IOException) {
+            // Manejar error al crear la base de datos
+            Log.e("MyRoute:DBManager/Update", "No se pudo crear la base de datos: " + e.message)
+            false
+        }
+    }
+
+    private fun modifyVersionTextFile(version: String) {
+        try {
+            val outputFile = File(context.filesDir, "version.txt")
+            outputFile.delete()
+
+            val outputStream = FileOutputStream(outputFile)
+            outputStream.write(version.toByteArray())
+            outputStream.close()
+
+            // El archivo ha sido modificado exitosamente
+        } catch (e: IOException) {
+            // Manejar error de lectura o escritura del archivo
+            Log.e("MyRoute:DBManager/Update", "Error al modificar el archivo: " + e.message)
+        }
+    }
 }
